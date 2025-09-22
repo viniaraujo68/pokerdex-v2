12267197f05803153fd76348995319a38212d4f8
viniaraujo68 <viniaraujoaraujo.va@gmail.com>
2025-09-21 23:54:27 -0300

    feat: add more CRUD operations for group and fix game_create_view checkbox problem

--- core/forms.py
+++ core/forms.py
@@ -1,15 +1,37 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from .models import Game, GameParticipation
+from .models import Game, GameParticipation, Group, GroupMembership
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.forms import UserCreationForm
 User = get_user_model()
 
 
 class GameForm(forms.ModelForm):
+    groups = forms.ModelMultipleChoiceField(
+        queryset=Group.objects.none(),
+        widget=forms.CheckboxSelectMultiple,
+        required=True,
+        label="",
+    )
+
     class Meta:
         model = Game
         fields = ["title", "date", "location", "buy_in"]
+
+    def __init__(self, *args, user=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["groups"].queryset = Group.objects.filter(
+            id__in=GroupMembership.objects.filter(user=user).values_list("group_id", flat=True)
+        )
+
+        if self.fields["groups"].queryset.exists():
+            self.fields["groups"].label = "Postar em grupos"
+
+    def clean_groups(self):
+        groups = self.cleaned_data.get("groups")
+        if not groups:
+            raise forms.ValidationError("A partida deve ser postada em pelo menos um grupo.")
+        return groups
 
 
 class GameParticipationForm(forms.ModelForm):
@@ -31,6 +53,13 @@ class LoginForm(AuthenticationForm):
     )
 
 class SignUpForm(UserCreationForm):
+    username = forms.CharField(
+        label="Usuário",
+        max_length=150,
+        help_text="Obrigatório. Até 150 caracteres. Letras, dígitos e @/./+/-/_ apenas.",
+        widget=forms.TextInput(attrs={"placeholder": "Seu usuário"})
+    )
+    
     email = forms.EmailField(
         required=True,
         help_text="Obrigatório. Informe um email válido."
